d36a4f5563b5bb7181539cec02c880964341d689
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-05-18 14:02:45 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let semaforo1 = ""
+let semaforo2 = ""
+led.enable(false)
 basic.forever(function () {
-	
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    semaforo2 = "rojo"
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    semaforo1 = "verde"
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    semaforo1 = "amarillo"
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(100)
+    }
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    semaforo2 = "rojo"
+    basic.pause(100)
 })
