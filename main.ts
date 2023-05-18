6d27a1b0a4b1173113fbe513033ab9c11423a82c
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-05-18 14:06:01 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,12 +10,26 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P13, 0)
     semaforo1 = "amarillo"
     for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P8, 1)
         basic.pause(100)
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
         basic.pause(100)
     }
     pins.digitalWritePin(DigitalPin.P2, 0)
     semaforo2 = "rojo"
     basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    semaforo1 = "rojo"
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    semaforo2 = "verde"
+    basic.pause(100)
+    semaforo1 = "amarillo"
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(100)
+    }
+    pins.digitalWritePin(DigitalPin.P9, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
 })
